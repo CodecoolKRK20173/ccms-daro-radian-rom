63ca7d48cb97f798f3248bfda99b9241a3acccce
Roman Bulinski <aboandrus@gmail.com>
2018-09-18 12:04:41 +0200

    Strated Student class

---
+++ src/main/java/com/codecool/Student.java
@@ -0,0 +1,52 @@
+package com.codecool;
+
+
+public class Student {
+
+    private String name;
+    private String surname;
+    private String[] listOfTakenAssaigments;
+    private String[] listOfSubmitetAssaigments;
+
+
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String[] getListOfTakenAssaigments() {
+        return listOfTakenAssaigments;
+    }
+
+    public String[] getListOfSubmitetAssaigments() {
+        return listOfSubmitetAssaigments;
+    }
+
+
+    public void login(){
+    }
+
+    public void takeAssignment(){
+
+    }
+
+    public void submitAssignment(){
+
+    }
+
+    public void viewMyGrades(){
+
+    }
+
+
+
+
+}
