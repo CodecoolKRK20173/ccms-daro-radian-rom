0446e21137f34a77611781ef62a09101f6601224
adrianOstromecki <bloodrian@gmail.com>
2018-09-19 13:40:19 +0200

    change field type of view from View to CCMSView

--- src/main/java/com/codecool/CCMS.java
+++ src/main/java/com/codecool/CCMS.java
@@ -6,26 +6,32 @@ import com.codecool.login.LoginController;
 import com.codecool.login.UserSwitch;
 import com.codecool.user.User;
 import com.codecool.user.UserController;
-import com.codecool.view.View;
+import com.codecool.view.CCMSView;
 
 public class CCMS {
     private LoginController loginController;
     private boolean isRunning;
     private final String[] MENU_OPTIONS = {"Login"};
     private final String ASK_FOR_INPUT = "Please enter a number of option from above list:";
-    private View view;
+    private final String OPTION_NOT_RECOGNIZED_MESSAGE = "Sorry, this option seems to be corrupted." +
+                                                         " Please, contact the administrator";
+
+    private CCMSView view;
     private User user;
     private UserController userController;
 
-    public CCMS(View view){
+    public CCMS(CCMSView view){
         loginController = new LoginController(view);
         this.view = view;
         isRunning = true;
     }
 
     public void run(){
+        view.clearScreen();
+        view.printAppTitle();
+        view.waitAWhile();
         while (isRunning){
-            if (loginController.isUserLogger()){
+            if (loginController.isUserLogged()){
                 userController.run();
             } else {
                 handleMenu();
@@ -34,25 +40,38 @@ public class CCMS {
     }
 
     private void handleMenu(){
+        view.clearScreen();
+        view.printAppTitle();
         view.printOptions(MENU_OPTIONS);
         int option = -1;
 
         do{
-            view.askForNumber(ASK_FOR_INPUT);
+            option = view.askForNumber(ASK_FOR_INPUT);
         } while (option < 0 || option > MENU_OPTIONS.length);
 
-        switch (option){
-            case 1:
-                handleLogin();
-                break;
-            case 0:
-                isRunning = false;
-                break;
+        if (option == view.getExitInput()){
+            isRunning = false;
+        } else {
+            switch (option){
+                case 1:
+                    handleLogin();
+                    break;
+                default:
+                    view.printError(OPTION_NOT_RECOGNIZED_MESSAGE);
+                    view.waitAWhile();
+                    break;
+            }
         }
     }
 
     private void handleLogin(){
         loginController.logIn();
+        if (loginController.isUserLogged()){
+            initializeUserController();
+        }
+    }
+
+    private void initializeUserController(){
         try{
             user = loginController.getLoggedUser();
         } catch (UserNotLoggedException e){
