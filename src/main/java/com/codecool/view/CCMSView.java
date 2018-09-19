afe28e37f8f908e885c153c288e261f0af6133e6
adrianOstromecki <bloodrian@gmail.com>
2018-09-19 13:46:06 +0200

    repair not-working delay function, add ASCII art printing

--- src/main/java/com/codecool/view/CCMSView.java
+++ src/main/java/com/codecool/view/CCMSView.java
@@ -2,13 +2,13 @@ package com.codecool.view;
 
 import java.util.List;
 
-public class CMSView extends View {
+public class CCMSView extends View {
     private final String APP_TITLE_FILE = "appTitle.txt";
 
     public void printAppTitle(){
         List<String> titleASCII = getASCIIArtFromFile(APP_TITLE_FILE);
         for (String line : titleASCII){
-            print(line);
+            println(line);
         }
     }
 }
