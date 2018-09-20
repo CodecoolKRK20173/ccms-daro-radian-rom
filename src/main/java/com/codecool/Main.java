b69cd0d04bd10b41d0027b96fc52bd7f59d48c52
Roman Bulinski <aboandrus@gmail.com>
2018-09-20 14:41:52 +0200

    Merge commit

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,23 +1,5 @@
 package com.codecool;
 
-<<<<<<< Temporary merge branch 1
-
-import com.codecool.DAO.AssignmentDAO;
-
-public class Main {
-    public static void main(String[] args){
-        AssignmentDAO assignmentDAO = new AssignmentDAO();
-        //assignmentDAO.addAssignment("dd", "klondike", "Jan Kowalski", "klondike game");
-        //assignmentDAO.addAssignment("dd2", "klondike 2", "Jan Kowalski", "klondike game2");
-        assignmentDAO.removeAssignment("dd2");
-
-=======
-import com.codecool.DAO.MentorsDAO;
-import com.codecool.DAO.StudentsDAO;
-import com.codecool.user.MentorController;
-import com.codecool.user.StudentController;
-import com.codecool.view.*;
-
 public class Main {
     public static void main(String[] args){
         CCMSConsoleView ccmsView = new CCMSConsoleView();
