7e790131b15464216222822f8ff6aeae8021530c
dardomanski <dardomanski@gmail.com>
2018-09-20 14:00:33 +0200

    implemented StudentPrintable interface

---
+++ src/main/java/com/codecool/view/StudentPrintable.java
@@ -0,0 +1,9 @@
+package com.codecool.view;
+
+import com.codecool.user.Student;
+
+import java.util.List;
+
+public interface StudentPrintable {
+    void PrintStudent(List<Student> studentList);
+}
